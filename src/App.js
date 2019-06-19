35b92e878c79c13ca7f6cfb82083a8b8ccbd472b
Frank Robenalt <frank.robenalt@gmail.com>
2019-06-18 22:12:24 -0400

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import InventoryContainer from './InventoryContainer/InventoryContainer.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Prints By Frank Inventory</div>
+      <InventoryContainer />
+    </div>
+  );
+}
+
+export default App;
