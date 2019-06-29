25d53b3aaa7953281b8d283186b272d9a5327cb0
Frank Robenalt <frank.robenalt@gmail.com>
2019-06-28 21:52:10 -0400

    prod

---
+++ build/precache-manifest.06c646bed46801466ebfaa5a72046852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f216d9696c09216a3b80de28c74ac695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "692a795440a7a7651af0",
+    "url": "/static/css/main.3a20d745.chunk.css"
+  },
+  {
+    "revision": "2ccb8c9d8404cb44224b",
+    "url": "/static/js/2.a3568ef9.chunk.js"
+  },
+  {
+    "revision": "692a795440a7a7651af0",
+    "url": "/static/js/main.0b2c45be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9806de5e29c11eb0e0888b6cd62d7659",
+    "url": "/static/media/printsbyfrank-inventory-manager.9806de5e.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -14,16 +14,11 @@ app.use( cors() );
 
 const massiveConnection = massive(process.env.CONNECTION_STRING).then(db=>app.set('db', db)).catch(err => console.log(err) );
 
-// app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/*', function(req, res) {
-//   console.log('here')
-//   res.sendFile(path.join(__dirname, 'dist'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // const server = http.createServer((req, res) => {
 //   res.statusCode = 200;
